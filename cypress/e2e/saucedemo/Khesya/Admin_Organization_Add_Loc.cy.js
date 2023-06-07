6a4cdd866b8c94e3e88392f4e33dcf45c1b27f89
khesya <azwakhesya28@gmail.com>
2023-06-07 12:14:36 +0700

    Organization Add Loc and Organization Edit Loc

--- cypress/e2e/saucedemo/Khesya/Admin_Organization_Add_Loc.cy.js
+++ cypress/e2e/saucedemo/Khesya/Admin_Organization_Add_Loc.cy.js
@@ -1,7 +1,7 @@
 import log_in from "../../../support/log_init.cy.js";
 const loginData = require("../../../fixtures/login/login.json");
 
-Cypress.Commands.add("login_org_loc", (email, password) => {
+Cypress.Commands.add("add_org_loc", (email, password) => {
   cy.visit("https://opensource-demo.orangehrmlive.com/");
   const log_main = new log_in();
   log_main.inputUsername(email);
@@ -16,31 +16,26 @@ Cypress.Commands.add("login_org_loc", (email, password) => {
 
 describe('Organization Add Location', () => {
     it('success add location', ()=>{
-        cy.login_org_loc(loginData.validUsername,loginData.validPassword);
-        cy.url().should('include', 'admin/viewLocations');
+      cy.add_org_loc(loginData.validUsername,loginData.validPassword);
+      cy.url().should('include', 'admin/viewLocations');
 
-        cy.get('.orangehrm-header-container > div > .oxd-button').click();
-		cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('Jeju');
-		cy.get(':nth-child(2) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Jeju');
-		cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('East Kalimantan');
-		cy.get('.oxd-select-text').click();
-		cy.contains('Argentina').click();
-		cy.get(':nth-child(5) > .oxd-input-group > :nth-child(2) > .oxd-input').type('089827355261');
-		cy.get('.oxd-button--secondary').click();
-		cy.get('.oxd-toast').should('contain','Successfully Saved');
+      cy.get('.orangehrm-header-container > div > .oxd-button').click();
+      cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('Jeju');
+      cy.get(':nth-child(2) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Jeju');
+      cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('East Kalimantan');
+      cy.get('.oxd-select-text').click();
+      cy.contains('Argentina').click();
+      cy.get(':nth-child(5) > .oxd-input-group > :nth-child(2) > .oxd-input').type('089827355261');
+      cy.get('.oxd-button--secondary').click();
+      cy.get('.oxd-toast').should('contain','Successfully Saved');
     })
-    it.only('success add location', ()=>{
-        cy.login_org_loc(loginData.validUsername,loginData.validPassword);
-        cy.url().should('include', 'admin/viewLocations');
+    it.only('failed add location', ()=>{
+      cy.add_org_loc(loginData.validUsername,loginData.validPassword);
+      cy.url().should('include', 'admin/viewLocations');
 
-        cy.get('.orangehrm-header-container > div > .oxd-button').click();
-		// cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('Jeju');
-		// cy.get(':nth-child(2) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Jeju');
-		// cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('East Kalimantan');
-		// cy.get('.oxd-select-text').click();
-		// cy.contains('Argentina').click();
-		// cy.get(':nth-child(5) > .oxd-input-group > :nth-child(2) > .oxd-input').type('089827355261');
-		cy.get('.oxd-button--secondary').click();
-		// cy.get('.oxd-toast').should('contain','Successfully Saved');
+      cy.get('.orangehrm-header-container > div > .oxd-button').click();
+		  cy.get('.oxd-button--secondary').click();
+      cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > .oxd-text').should('contain','Required');
+      cy.get(':nth-child(4) > .oxd-input-group > .oxd-text').should('contain','Required');
     })
 })
---
+++ cypress/e2e/saucedemo/Khesya/Admin_Organization_Edit_Loc.cy.js
@@ -0,0 +1,41 @@
+import log_in from "../../../support/log_init.cy.js";
+const loginData = require("../../../fixtures/login/login.json");
+
+Cypress.Commands.add("edit_org_loc", (email, password) => {
+  cy.visit("https://opensource-demo.orangehrmlive.com/");
+  const log_main = new log_in();
+  log_main.inputUsername(email);
+  log_main.inputPassword(password);
+  log_main.clickLogin();
+  log_main.verifyDashboard();
+  log_main.verifyUrl();
+  cy.get(':nth-child(1) > .oxd-main-menu-item').click();
+  cy.get(':nth-child(3) > .oxd-topbar-body-nav-tab-item').click();
+  cy.get('.oxd-dropdown-menu > :nth-child(2)').click();
+})
+
+describe('Organization Edit Location', () => {
+    it('success edit location', ()=>{
+      cy.edit_org_loc(loginData.validUsername,loginData.validPassword);
+      cy.url().should('include', 'admin/viewLocations');
+
+      cy.get(':nth-child(1) > .oxd-table-row > :nth-child(7) > .oxd-table-cell-actions > :nth-child(2)').click({force:true}); // click Edit 
+		  cy.get(':nth-child(2) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear()
+      cy.get(':nth-child(2) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Vancouver');
+      cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').clear()
+      cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('442');
+
+		  cy.get('.oxd-button--secondary').click();
+		  cy.get('.oxd-toast').should('contain','Successfully Updated');
+    })
+    it('failed edit location', ()=>{
+      cy.edit_org_loc(loginData.validUsername,loginData.validPassword);
+      cy.url().should('include', 'admin/viewLocations');
+
+      cy.get(':nth-child(1) > .oxd-table-row > :nth-child(7) > .oxd-table-cell-actions > :nth-child(2)').click({force:true}); // click Edit   
+		  cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear();
+
+		  cy.get('.oxd-button--secondary').click();
+		  cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > .oxd-text').should('contain','Required');
+    })
+})
